eb36260d1a128ce284da90fafcda3d180f025dbf
dirkkerber <dirkkerber@gmx.de>
2011-05-16 13:17:44 +0200

    Unit 6 passed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 SampleApp::Application.routes.draw do
+  resources :users
+  
+  get "users/new"
   get "pages/home"
   get "pages/contact"
   get "pages/about"
+  get "pages/help"
+  
+  root :to => 'pages#home'
+  match '/', :to => 'pages#home'
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
+  match '/new', :to => 'users#new' 
+  match '/signup',  :to => 'users#new'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
